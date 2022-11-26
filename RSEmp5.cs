97e0cc9e76d1b634a8abf94b5d5eebcd945e2c0e
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-11-26 20:52:21 +0530

    Delete Employee Details By Id Using RestSharp
    Delete Employee Details By Id Using RestSharp

---
+++ RSEmp5.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using System;
+
+namespace RSEmp5
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+    public class Operation
+    {
+        RestClient restClient = new RestClient("http://localhost:4000");
+        
+        public void DeleteEmployee()
+        {
+            RestRequest rq = new RestRequest("/employee/109", Method.DELETE);
+            restClient.Delete(rq);
+        }
+    }
+    public class RSEmp5
+    {
+        static void Main(string[] args)
+        {
+            Operation obj= new Operation();
+            obj.DeleteEmployee();
+        }
+    }
+}
